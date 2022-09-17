37c4160ffe4d82f0a406806f77babb7e206e208d
azmu0520 <azizbekbootcam@gmail.com>
2022-09-17 17:17:21 +0500

    set up done

---
+++ src/utilits/sidebar.js
@@ -0,0 +1,58 @@
+import Generic from '../components/Generic';
+
+export const data = [
+  {
+    id: 1,
+    title: 'Dashboard',
+    path: '/home',
+    Component: Generic,
+  },
+  {
+    id: 2,
+    title: "O'quvchilar",
+    path: '/students',
+    Component: Generic,
+  },
+  {
+    id: 3,
+    title: 'Lidlar',
+    path: '/leaders',
+    Component: Generic,
+  },
+  {
+    id: 4,
+    title: 'Guruhlar',
+    path: '/groups',
+    Component: Generic,
+  },
+  {
+    id: 5,
+    title: 'Vazifalar',
+    path: '/homewors',
+    Component: Generic,
+  },
+  {
+    id: 6,
+    title: 'Calendar',
+    path: '/calendar',
+    Component: Generic,
+  },
+  {
+    id: 7,
+    title: 'Moliya',
+    path: '/balance',
+    Component: Generic,
+  },
+  {
+    id: 8,
+    title: 'Bo‘limlar',
+    path: '/sections',
+    Component: Generic,
+  },
+  {
+    id: 9,
+    title: 'Sozlamar',
+    path: '/settings',
+    Component: Generic,
+  },
+];
