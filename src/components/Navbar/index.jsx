2391fbb95812e534d4fb7182592c8abbedee17b7
azmu0520 <azizbekbootcam@gmail.com>
2022-09-17 19:14:07 +0500

    done

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Wrap, Nav, Rest } from './style';
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import { data } from '../../utilits/sidebar';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const location = useLocation();
 
-export default Navbar
+  return (
+    <Wrap>
+      <Nav>
+        <Nav.Logo_Wrap>
+          <Nav.Logo />
+          <Nav.Logo_Text>Webbrain Academy</Nav.Logo_Text>
+        </Nav.Logo_Wrap>
+        <Nav.Links>
+          {data.map(({ id, title, path, Icon }) => (
+            <NavLink key={id} to={path}>
+              <Nav.Link active={(location.pathname == path).toString()}>
+                <Icon className='nav__icon' />
+                <Nav.Link_Text active={(location.pathname == path).toString()}>
+                  {title}
+                </Nav.Link_Text>
+              </Nav.Link>
+            </NavLink>
+          ))}
+        </Nav.Links>
+      </Nav>
+      <Rest>
+        <Outlet />
+      </Rest>
+    </Wrap>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Navbar/style.js
@@ -0,0 +1,95 @@
+import styled from 'styled-components';
+import { ReactComponent as Logo } from '../../assets/icons/logo.svg';
+export const Wrap = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Nav = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  padding: 40px 32px;
+  background: #ffffff;
+  box-shadow: 6px 0px 18px rgba(0, 0, 0, 0.06);
+
+  @media (max-width: 1400px) {
+  }
+  @media (max-width: 765px) {
+    padding: 25px 16px;
+  }
+  @media (max-width: 345px) {
+  }
+`;
+
+Nav.Logo_Wrap = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: fit-content;
+  margin-bottom: 45px;
+  @media (max-width: 765px) {
+    justify-content: center;
+  }
+`;
+
+Nav.Logo_Text = styled.span`
+  margin: 0 15px;
+  font-size: 20px;
+  font-weight: 600;
+  color: var(--color-primary);
+  @media (max-width: 765px) {
+    display: none;
+  }
+`;
+
+Nav.Logo = styled(Logo)`
+  width: 40px;
+  /* width: 133px;
+  height: fit-content;
+  margin-bottom: 45px;
+  cursor: pointer; */
+`;
+
+Nav.Links = styled.ul`
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid #ebeff2;
+`;
+
+Nav.Link = styled.li`
+  display: flex;
+  align-items: center;
+  .h3 {
+    margin-left: 16px;
+  }
+  .nav__icon {
+    path {
+      fill: ${({ active }) =>
+        active == 'true' ? 'var(--color-primary)' : '#334D6E'};
+      transition: all 0.3s ease-in-out;
+    }
+  }
+  @media (max-width: 765px) {
+    justify-content: center;
+  }
+  margin-bottom: 38px;
+`;
+
+Nav.Link_Text = styled.h3`
+  margin-left: 16px;
+  color: ${({ active }) =>
+    active == 'true' ? 'var(--color-primary)' : '#334D6E'};
+  transition: all 0.3s ease-in-out;
+  @media (max-width: 765px) {
+    display: none;
+  }
+`;
+
+export const Rest = styled.div`
+  display: flex;
+  flex: 4.2;
+`;
