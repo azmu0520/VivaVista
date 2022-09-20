f27914f256f112cafe37fd20247a21cfdd2d8199
azmu0520 <azizbekbootcam@gmail.com>
2022-09-20 18:21:13 +0500

    done

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Wrap, Nav, Rest } from './style';
 import { NavLink, Outlet, useLocation } from 'react-router-dom';
 import { data } from '../../utilits/sidebar';
+import user from '../../assets/images/user.png'
 
 const Navbar = () => {
   const location = useLocation();
@@ -30,6 +31,17 @@ const Navbar = () => {
         </Nav.Links>
       </Nav>
       <Rest>
+        <Rest.Searchbar>
+          <Rest.InputWrap>
+            <Rest.SearchIcon />
+            <Rest.Input placeholder='Reference search' />
+          </Rest.InputWrap>
+          <Rest.Notify>
+            <Rest.Notification />
+            <span className='notifies__number'>1</span>
+          </Rest.Notify>
+          <Rest.User src={user} />
+        </Rest.Searchbar>
         <Outlet />
       </Rest>
     </Wrap>
