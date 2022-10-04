7a142ee38835bb51a0286689b46a5ba95926e0f9
azmu0520 <azizbekbootcam@gmail.com>
2022-10-04 23:31:25 +0500

    done

--- src/components/Generic/Table/style.js
+++ src/components/Generic/Table/style.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
-
+import { ReactComponent as Delete } from '../../../assets/icons/delete.svg';
+import { ReactComponent as Edit } from '../../../assets/icons/edit.svg';
 export const Grid = styled.div`
   margin: 15px 0;
+  position: relative;
+  padding-top: 50px;
 `;
 Grid.Data = styled.div`
   font-weight: 500;
@@ -17,14 +20,52 @@ Grid.Data = styled.div`
 `;
 Grid.Row = styled.div`
   display: grid;
+  position: relative;
   grid-template-columns: 0.2fr 1fr 1fr 1fr 1fr 1fr;
   height: 55px;
   align-items: center;
+  z-index: 10;
   .row__input:checked ~ ${Grid.Data} {
     color: #29a0dc;
 
     opacity: 1;
   }
 
+  .header__action {
+    position: absolute !important;
+  }
+
   padding-left: 30px;
+`;
+
+Grid.Row_Btn = styled.div`
+  width: 123px;
+  height: 36px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #f5f6f7;
+  border-radius: 4px;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  color: #979ba5;
+  margin-right: 16px;
+  cursor: pointer;
+`;
+
+Grid.Header = styled.div`
+  display: flex;
+  position: absolute;
+  top: 0;
+  left: 30px;
+`;
+
+export const Icon = styled.div``;
+
+Icon.Delete = styled(Delete)`
+  margin-right: 6px;
+`;
+Icon.Edit = styled(Edit)`
+  margin-right: 6px;
 `;
